8b3007a2b78114ffa9f195c3019455ad3a576f91
Mathieu Lavoie <mathieulavoie94@gmail.com>
2020-12-19 13:29:24 -0500

    feat: typescript support

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,30 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = () => {
   return {
+    entry: './src/index.ts',
     mode: 'production', 
     plugins: [
       new HtmlWebpackPlugin({
         template: './src/index.html' 
       })
     ],
+    module: {
+      rules: [
+        {
+          test: /\.ts?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: [ '.ts', '.js' ],
+    },
+    output: {
+      filename: 'index.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
   };
 };
