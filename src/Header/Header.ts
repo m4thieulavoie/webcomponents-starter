1b53eda7278ea2aad3c7a48938807d8dc88f1da5
Mathieu Lavoie <mathieulavoie94@gmail.com>
2020-12-28 15:22:31 -0500

    feat: some docs

--- src/Header/Header.ts
+++ src/Header/Header.ts
@@ -2,7 +2,7 @@ import { FASTElement, customElement, html } from "@microsoft/fast-element";
 import styles from "./Header.scss";
 
 const template = html`
-  <a href="/">Home</a><a href="/nsdfoig">Random non existing page</a>
+  <a href="/">Home</a><a href="/nsdfoig">Random non existing page</a><a href="https://github.com/m4thieulavoie/webcomponents-starter" target="_blank">View in GitHub</a>
 `;
 
 @customElement({
