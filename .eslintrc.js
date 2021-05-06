2a2a55f4749588af600882f0bf9c96c02463b50b
Mathieu Lavoie <mathieu.lavoie@logmein.com>
2021-05-06 14:18:54 -0400

    now using linting package

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,3 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: ["airbnb-base", "prettier"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: "module",
-  },
-  plugins: ["@typescript-eslint"],
-  rules: {
-    "import/no-unresolved": 0,
-    "no-irregular-whitespace": 0,
-    "import/extensions": 0,
-  },
+  extends: require.resolve("@m4thieulavoie/linting"),
 };
